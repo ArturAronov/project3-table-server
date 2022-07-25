040bc39588d545966f5a0041d60d6a1c8ceebcdd
ArturAronov <unionco@icloud.com>
2022-07-25 23:07:34 +0800

    finished the show profile APIs

---
+++ src/controllers/api/business/profile/index.js
@@ -0,0 +1,29 @@
+import _ from 'lodash'
+
+import handleErrors from '../../../_helpers/handle-errors.js'
+import prisma from '../../../_helpers/prisma.js'
+
+const controllersApiBusinessProfileIndex = async (req, res) => {
+  try {
+    // console.log(req.session.user.id)
+    const { session: {
+      user: {
+        id
+      }
+    }
+    } = req
+
+    const getProfile = await prisma.restaurant.findUnique({
+      where: {
+        id
+      },
+      rejectOnNotFound: true
+    })
+
+    return res.status(201).json(_.omit(getProfile, ['passwordHash']))
+  } catch (err) {
+    return handleErrors(res, err)
+  }
+}
+
+export default controllersApiBusinessProfileIndex
---
+++ src/controllers/api/user/profile/index.js
@@ -0,0 +1,29 @@
+import _ from 'lodash'
+
+import handleErrors from '../../../_helpers/handle-errors.js'
+import prisma from '../../../_helpers/prisma.js'
+
+const controllersApiUserProfileIndex = async (req, res) => {
+  try {
+    // console.log(req.session.user.id)
+    const { session: {
+      user: {
+        id
+      }
+    }
+    } = req
+
+    const getProfile = await prisma.user.findUnique({
+      where: {
+        id
+      },
+      rejectOnNotFound: true
+    })
+
+    return res.status(201).json(_.omit(getProfile, ['passwordHash']))
+  } catch (err) {
+    return handleErrors(res, err)
+  }
+}
+
+export default controllersApiUserProfileIndex
