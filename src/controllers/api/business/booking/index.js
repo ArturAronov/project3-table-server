2b2aa7b92d776d9095d6a983018ca90bf416175a
ArturAronov <unionco@icloud.com>
2022-07-28 10:05:34 +0800

    completed business get bookings API

---
+++ src/controllers/api/business/booking/index.js
@@ -0,0 +1,20 @@
+import prisma from '../../../_helpers/prisma.js'
+import handleErrors from '../../../_helpers/handle-errors.js'
+
+const controllersApiBusinessBookingIndex = async (req, res) => {
+  try {
+    const restaurantId = req.session.restaurant.id
+
+    const getBookings = await prisma.booking.findMany({
+      where: {
+        restaurantId
+      }
+    })
+
+    return res.status(200).json(getBookings)
+  } catch (err) {
+    handleErrors(res, err)
+  }
+}
+
+export default controllersApiBusinessBookingIndex
