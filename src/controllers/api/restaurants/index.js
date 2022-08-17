6690c5f62038f9006f0590541dd9a22d4a68e952
ArturAronov <unionco@icloud.com>
2022-08-18 01:51:54 +0800

    removed paswordHash from return

--- src/controllers/api/restaurants/index.js
+++ src/controllers/api/restaurants/index.js
@@ -1,11 +1,13 @@
+import _ from 'lodash'
 import handleErrors from '../../_helpers/handle-errors.js'
 import prisma from '../../_helpers/prisma.js'
 
 const controllersApiRestaurantsIndex = async (req, res) => {
   try {
     const getRestaurants = await prisma.restaurant.findMany()
+    const removeHash = await getRestaurants.map((element) => _.omit(element, ['passwordHash']))
 
-    return res.status(201).json(getRestaurants)
+    return res.status(201).json(removeHash)
   } catch (err) {
     return handleErrors(res, err)
   }
